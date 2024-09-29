bf25d6406af5c69d6b94ba6394ee74d23215f3f9
Oona <oona.e.jaakkola@gmail.com>
2024-09-29 13:02:06 +0300

    Exercise complete

---
+++ src/todolist.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+function TodoList() {
+  const [todo, setTodo] = useState({description: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+  }
+
+
+  return (
+    <>
+      <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
+      <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
+      <button onClick={addTodo}>Add</button>
+      <table>
+        <tbody>
+       {
+          todos.map((todo, index) => 
+            <tr key={index}>
+              <td>{todo.description}</td>
+             <td>{todo.date}</td>
+            </tr>)
+        }
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default TodoList;
